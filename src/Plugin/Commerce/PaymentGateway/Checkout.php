3ab721e20842749ea4b96d747e4fa55487665750
Jonathan Sacksick <jonathan.sacksick@gmail.com>
2019-03-26 17:46:17 +0100

    Issue #3043123 by jsacksick: Add access requirements to to the createOrder + onApprove routes.

--- src/Plugin/Commerce/PaymentGateway/Checkout.php
+++ src/Plugin/Commerce/PaymentGateway/Checkout.php
@@ -575,6 +575,8 @@ class Checkout extends OnsitePaymentGatewayBase implements CheckoutInterface {
     if (strtolower($response['status']) !== 'completed') {
       throw new PaymentGatewayException(sprintf('Invalid state returned by PayPal. Expected: ("%s"), Actual: ("%s").', 'COMPLETED', $response['status']));
     }
+    $payment->setRemoteId($response['id']);
+    $payment->setRemoteState($response['status']);
     $payment->setRefundedAmount($new_refunded_amount);
     $payment->save();
   }
